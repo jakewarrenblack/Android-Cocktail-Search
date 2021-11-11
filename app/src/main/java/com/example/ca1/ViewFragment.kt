295b466c51a9e30e7d50f2720217f2fd607e6866
Jake <jakewarrenblack01@gmail.com>
2021-11-11 22:43:16 +0000

    Added ic_check icon. Handling clicks to both the custom back icon, as well as the device back gesture. Clicking either of these returns the user back to the main fragment and hides the back icon.

--- app/src/main/java/com/example/ca1/ViewFragment.kt
+++ app/src/main/java/com/example/ca1/ViewFragment.kt
@@ -4,8 +4,12 @@ import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.ca1.databinding.ViewFragmentBinding
 
@@ -21,6 +25,21 @@ class ViewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // make the back icon disappear when not on the single page
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+        // get a reference to the activity which owns this fragment
+        (activity as AppCompatActivity).supportActionBar?.let{
+            it.setHomeButtonEnabled(true)
+            it.setDisplayShowHomeEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_check)
+        }
+
+        setHasOptionsMenu(true)
+
+
         // initialise the binding
         binding = ViewFragmentBinding.inflate(inflater, container, false);
         // use this binding to update the TextView
@@ -29,8 +48,33 @@ class ViewFragment : Fragment() {
         binding.cocktailText.setText("You selected cocktail number ${args.cocktailId}")
         // this file uses the view_fragment.xml as its layout file
 
+        // if the user runs a back gesture either through an actual button OR a gesture,
+        // handle it by calling the custom saveAndReturn() function which will result
+        // in navigating up using the navigationController
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object: OnBackPressedCallback(true){
+                override fun handleOnBackPressed() {
+                    saveAndReturn()
+                }
+            }
+        )
+
         // we've already inflated the layout, so we'll just return the binding.root instead of returning the inflated layout
         return binding.root
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean{
+        return when (item.itemId){
+            android.R.id.home -> saveAndReturn()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun saveAndReturn(): Boolean {
+        findNavController().navigateUp()
+        return true
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
