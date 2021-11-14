cc6701fad6539e31f32e1ad6cc36e7ce715951b1
Jake <jakewarrenblack01@gmail.com>
2021-11-14 22:09:40 +0000

    Add room library to dependencies. Convert cocktailEntity class to room data entity.

--- app/src/main/java/com/example/ca1/data/CocktailEntity.kt
+++ app/src/main/java/com/example/ca1/data/CocktailEntity.kt
@@ -1,9 +1,15 @@
 package com.example.ca1.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.ca1.NEW_COCKTAIL_ID
 import java.util.*
 
+// we could choose not to provide a tableName, in which case
+// the table would just be called 'CocktailEntity'
+@Entity(tableName = "cocktails")
 data class CocktailEntity (
+    @PrimaryKey(autoGenerate = true)
     var id:Int,
     var date: Date,
     var text: String
